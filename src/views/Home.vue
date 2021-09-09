cc36f23a2172a82462a3d1ec5351b1ba44f9e51b
Victor Gualdron Ruiz <victor.gualdron@masiv.com>
2021-09-09 17:59:01 -0500

    ajustes de front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,22 @@
 		          tag="article"
 		          class="m-3 mt-3">
 		          <b-row>
-								<b-col cols="12" sm="6" lg="3" v-if="aplicaciones.length > 0" v-for="aplicacion in aplicaciones" :key="'apli_' + aplicacion.id">
-									<b-card :no-body="true" footer-class="px-3 py-2">
-										<b-card-body class="p-3 clearfix">
-											<i :class="aplicacion.icono + ' bg-primary p-3 font-2xl mr-3 float-left'"></i>
-											<div class="h5 text-primary mb-0 mt-2">{{aplicacion.descripcion}}</div>
-											<div class="text-muted text-uppercase font-weight-bold font-xs">{{aplicacion.descripcion}}</div>
-										</b-card-body>
-										<div slot="footer">
-											<b-link class="font-weight-bold font-xs btn-block text-muted" target="_blank" :href="aplicacion.url">Abrir Modulo <i class="fa fa-angle-right float-right font-lg"></i></b-link>
-										</div>
-									</b-card>
-								</b-col>
-								<b-col cols="12" sm="6" lg="3" v-else>
-									No tiene aplicaciones asiganadas
-								</b-col>
-							</b-row><!--/.row-->
+					<b-col cols="12" sm="6" lg="3" v-if="aplicaciones.length > 0" v-for="aplicacion in aplicaciones" :key="'apli_' + aplicacion.id">
+						<b-card :no-body="true" footer-class="px-3 py-2">
+							<b-card-body class="p-3 clearfix">
+								<i :class="aplicacion.icono + ' bg-primary p-3 font-2xl mr-3 float-left'"></i>
+								<div class="h5 text-primary mb-0 mt-2">{{aplicacion.descripcion}}</div>
+								<div class="text-muted text-uppercase font-weight-bold font-xs">{{aplicacion.descripcion}}</div>
+							</b-card-body>
+							<div slot="footer">
+								<b-link class="font-weight-bold font-xs btn-block text-muted" target="_blank" :href="aplicacion.url">Abrir Modulo <i class="fa fa-angle-right float-right font-lg"></i></b-link>
+							</div>
+						</b-card>
+					</b-col>
+					<b-col cols="12" sm="6" lg="3" v-else>
+						No tiene aplicaciones asiganadas
+					</b-col>
+				</b-row><!--/.row-->
 		  		</b-card>
 			</b-col>
 		</b-row>
