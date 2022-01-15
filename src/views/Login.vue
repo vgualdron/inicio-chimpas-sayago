98df187fdf61d30c8b26cf424a33e8554cf50327
Victor Gualdron <victor.gualdron.r@gmail.com>
2022-01-15 14:56:41 -0500

    changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,7 @@
       <b-col></b-col>
       <b-col sm="10" md="8" lg="6" xl="5">
         <b-card class="animated fadeIn p-4">
-          <b-img style="width: 100%;" src="static/img/logo_login.jpg" class="img-responsive" alt="Washinhouse"/>
+          <b-img style="width: 80%;" src="static/img/logo_login.jpg" class="img-responsive" alt="Washinhouse"/>
           <p class="font-weight-light" style="font-size:40px;">Bienvenido</p>
           <p class="text-muted">Ingrese su cuenta</p>
           <div class="mb-3">
